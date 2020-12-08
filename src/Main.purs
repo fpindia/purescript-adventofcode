4c46e7b9ebc3bcc9e5cfcde0c73ba08ec3fa272d
Anupam Jain <ajnsit@gmail.com>
2020-12-08 19:58:22 +0530

    Update instructions in Main.purs

--- src/Main.purs
+++ src/Main.purs
@@ -1,8 +1,12 @@
 module Main where
 
+import Control.Bind (discard)
 import Data.Unit (Unit)
 import Effect (Effect)
 import Effect.Class.Console (log)
 
 main :: Effect Unit
-main = log "Hello! You have successfully built the project! Start by running './aoc'"
+main = do
+  log "Hello! You have successfully built the project!"
+  log "This code resides in `src/Main.purs`. Feel free to delete/modify it as per your needs."
+  log "Start by running './aoc'"
